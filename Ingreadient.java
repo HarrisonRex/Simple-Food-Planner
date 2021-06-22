7c0ee405f361a4143e2d9c79efb7359f96e7721b
HarrisonRex <harrisonmacdonald79@googlemail.com>
2021-06-22 14:08:37 +0100

    init commit

---
+++ Ingreadient.java
@@ -0,0 +1,37 @@
+public class Ingreadient {
+	private boolean inStock;
+	private String name;
+	private double amountStock;
+	
+	public Ingreadient(boolean inStock, String name, double amountStock){
+		this.inStock = false;
+		this.name = name;
+		this.amountStock = amountStock;
+	}
+	
+	public void set(boolean inStock) {
+		this.inStock = inStock;
+	}
+	public void setName(String nName) {
+		this.name = name;
+	}
+	public void setAmountStock(double amountStock) {
+		this.amountStock = amountStock;
+	}
+	
+	public boolean getInStock() {
+		return(this.inStock);
+	}
+	public String getName() {
+		return(this.name);
+	}
+	public double getAmountStock() {
+		return(this.amountStock);
+	}
+	
+	public String toString() {
+		return(Boolean.toString(this.inStock) + "," + name + "," + amountStock);
+	}
+}
+	
+
